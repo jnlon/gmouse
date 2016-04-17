6079ce4d6ee13e21a0e5ab97ec80cfe429a96bec
spinclick <spinclick@users.noreply.github.com>
2016-04-17 19:02:46 -0400

    Early code structuring

--- gmouse.ino
+++ gmouse.ino
@@ -1,12 +1,59 @@
 #include <Mouse.h>
 #include <Wire.h>
 
-void setup() {
-  Wire.begin();
-  Wire.beginTransmission(104);
-  Wire.write(0x6B);  // PWR_MGMT_1 register
-  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+#include <gyroscope_registers.h>
+
+/*
+ * A note on setup and how communicating with the gyroscope works:
+ * 
+ * We can configure how the gyroscope functions by setting a value to its various "registers",
+ * which are basically memory cells that reside on the chip itself. We can read/write
+ * to these registers by sending the right signals to the chip over I2C (with Arduino's Wire library).
+ * It's important to note that mpu6050 AUTO INCREMENTS the register of the values
+ * we write, so we can theoretically setup or read from the entire chip at once with a single transmission.
+ * 
+ * Here is an annotated example of setting up the chip.
+ * 
+ * Wire.beginTransmission(SLAVE_ADDRESS); 
+ * // beginTransmission sends the I2C "START" signal. 
+ * // The SLAVE_ADDRESS is a value that uniquely identifies a component. 
+ * // The documentation says it is always 110100X (104 or 105) for mpu6050
+ *
+ * Wire.write(REGISTER_START_ADDRESS); 
+ * // Select the register at REGISTER_START_ADDRESS. On the mpu6050, registers 13 through 117 are available
+ * // Each of these registers has a unique purpose. See the register documentation for a description what it does
+ * 
+ * Wire.write(VALUE1); 
+ * // Set the value for REGISTER_START_ADDRESS
+ * // What each value does depends on the register, so see the documentation
+ * 
+ * Wire.write(VALUE2); 
+ * // Since the mpu6050 auto-increments the selected register, subsequent writes  
+ * // set the the value for the next register. So here, VALUE2 becomes the value of REGISTER_START_ADDRESS+1
+ * 
+ * ...
+ * Wire.endTransmission(true); 
+ * // Tell the gyroscope  we are done reading/writing values 
+ * // by sending the I2C "STOP" message (this is why we need (true) as the paramater)
+ * 
+ */
+
+
+#define MPU_6050_ADDRESS 104
+
+void set_register(byte reg, byte value) {
+  Wire.beginTransmission(MPU_6050_ADDRESS); 
+  Wire.write(reg);
+  Wire.write(value); 
   Wire.endTransmission(true);
+}
+
+void setup() {
+  
+  Wire.begin();
+
+  set_register(REG_PWR_MGMT_1, B1000); // Disable temperature sensor
+  
   Serial.begin(9600);
 }
 
---
+++ mpu6050_registers.h
@@ -0,0 +1,1 @@
+#define REG_PWR_MGMT_1 107
