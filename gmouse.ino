1829a3277058c42ae11de28025f82134bcb2633a
spinclick <spinclick@users.noreply.github.com>
2016-04-21 00:22:33 -0400

    Add logging functions

--- gmouse.ino
+++ gmouse.ino
@@ -15,8 +15,8 @@ enum click_state {
 };
 
 typedef struct mouse_state_s {
-  byte velocity_x; // Left/Right speed
-  byte velocity_y; // Up/Down speed
+  short velocity_x; // Left/Right speed
+  short velocity_y; // Up/Down speed
   bool scroll;    // Toggles scroll mode 
   click_state mouse1;  // Left click
   click_state mouse2;  // Right click
@@ -30,17 +30,32 @@ void set_register(byte reg, byte value) {
   Wire.endTransmission(true);
 }
 
+/* Return a mouse state with default values */
+mouse_state initial_mouse_state() {
+
+  mouse_state state;
+
+  state.velocity_x = 0;
+  state.velocity_y = 0;
+  state.scroll = false;
+  state.mouse1 = released;
+  state.mouse2 = released;
+
+  return state;
+}
+
+
 /* Sets the current state of the mouse. Checks to see if buttons are
  * pressed, what the gyroscope values are, etc*/
 mouse_state get_mouse_state(gyro_state gstate) {
 
-  struct mouse_state_s mstate;
+  mouse_state state = initial_mouse_state();
 
   /*TODO: 
     - Process gyroscope values (what are the ranges?), 
     - Figure out which pins do what (ie, clicking)*/
 
-  return mstate;
+  return state;
 
 }
 
@@ -53,6 +68,7 @@ void set_cursor_state(mouse_state state) {
 
 }
 
+/* Get X/Y/Z values from the gyroscope */
 gyro_state get_gyro_xyz() {
 
   Wire.beginTransmission(MPU_6050_ADDRESS);
@@ -70,28 +86,62 @@ gyro_state get_gyro_xyz() {
   state.z = GyZ;
 
   return state;
-
 }
+
+
+/* Print the state of the gyroscope */
+void print_gyro_state(gyro_state state) {
+  Serial.print("GyX = "); Serial.print(state.x);
+  Serial.print(" | GyY = "); Serial.print(state.y);
+  Serial.print(" | GyZ = "); Serial.print(state.z);
+  Serial.println("");
+}
+
+String click_to_string(click_state s) {
+  if (s == pressed)
+    return "pressed";
+  else if (s == released)
+    return "released";
+  return "?";
+}
+
+char bool_to_char(bool b) {
+  if (b)
+    return 'T';
+  else
+    return 'F';
+}
+
+void print_mouse_state(mouse_state state) {
+  Serial.println("");
+  Serial.print("velocity_x = "); Serial.println(state.velocity_x);
+  Serial.print("velocity_y = "); Serial.println(state.velocity_y);
+  Serial.print("scroll_on  = "); Serial.println(bool_to_char(state.scroll));
+  Serial.print("mouse1     = "); Serial.println(click_to_string(state.mouse1));
+  Serial.print("mouse2     = "); Serial.println(click_to_string(state.mouse2));
+  Serial.println("");
+}
+
 
 void setup() {
   
   Wire.begin();
   Serial.begin(9600);
+  Mouse.begin();
 
   // Disable temperature sensor 
   set_register(REG_PWR_MGMT_1, B1000);
 
   // Set FS_SEL to +- 2000 (this seems to reduce sensitivity)
   set_register(REG_GYRO_CONFIG, B11000); 
-  
 }
 
 void loop() {
-  gyro_state_s state = get_gyro_xyz();
-  
-  Serial.print("GyX = "); Serial.print(state.x);
-  Serial.print(" | GyY = "); Serial.print(state.y);
-  Serial.print(" | GyZ = "); Serial.println(state.z);
+  gyro_state gstate = get_gyro_xyz();
+  mouse_state mstate = get_mouse_state(gstate);
+
+  print_gyro_state(gstate);
+  print_mouse_state(mstate);
 
   delay(50);  
 }
